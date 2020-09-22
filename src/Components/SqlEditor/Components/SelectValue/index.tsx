03dd5e12ebb0538db0e40cacc00c91697ae9392a
缪宇 <1337611267@qq.com>
2020-09-22 17:02:54 +0800

    add some data

--- src/Components/SqlEditor/Components/SelectValue/index.tsx
+++ src/Components/SqlEditor/Components/SelectValue/index.tsx
@@ -48,4 +48,87 @@ const SelectValue: React.FC<SelectValueProps> = (props) => {
   );
 };
 
+const defaultData = [
+  {
+    name: "id",
+    dataType: "String",
+    portName: "dataset",
+    alias: "id",
+    selected: false,
+    comment: "组件ID"
+  },
+  {
+    name: "code",
+    dataType: "String",
+    portName: "dataset",
+    alias: "code",
+    selected: false,
+    comment: "算法编码"
+  },
+  {
+    name: "grp_id",
+    dataType: "String",
+    portName: "dataset",
+    alias: "grp_id",
+    selected: false,
+    comment: "组件属于的分组ID"
+  },
+  {
+    name: "name",
+    dataType: "String",
+    portName: "dataset",
+    alias: "name",
+    selected: false,
+    comment: "组件名称"
+  },
+  {
+    name: "sort_type",
+    dataType: "String",
+    portName: "dataset",
+    alias: "sort_type",
+    selected: false,
+    comment: "算法分类"
+  },
+  {
+    name: "data",
+    dataType: "String",
+    portName: "dataset",
+    alias: "data",
+    selected: false,
+    comment: "算法json参数"
+  },
+  {
+    name: "description",
+    dataType: "String",
+    portName: "dataset",
+    alias: "description",
+    selected: false,
+    comment: "组件简单描述"
+  },
+  {
+    name: "create_time",
+    dataType: "DateTime",
+    portName: "dataset",
+    alias: "create_time",
+    selected: false,
+    comment: "创建时间"
+  },
+  {
+    name: "user_id",
+    dataType: "String",
+    portName: "dataset",
+    alias: "user_id",
+    selected: false,
+    comment: "用户id"
+  },
+  {
+    name: "update_time",
+    dataType: "DateTime",
+    portName: "dataset",
+    alias: "update_time",
+    selected: false,
+    comment: "更新时间"
+  }
+];
+
 export default SelectValue;
--- src/Components/SqlEditor/config.ts
+++ src/Components/SqlEditor/config.ts
@@ -2,3 +2,46 @@ export const logicalOperatorsList = {
   and: "与",
   or: "或"
 };
+
+export const comparisonPperatorsList = {
+  int: [
+    { id: "<=", label: "小于等于" },
+    { id: ">=", label: "大于等于" },
+    { id: "<", label: "小于" },
+    { id: ">", label: "大于" },
+    { id: "!=", label: "不等于" },
+    { id: "=", label: "等于" },
+    { id: "in", label: "属于" },
+    { id: "not in", label: "不属于" },
+    { id: "is null", label: "空" },
+    { id: "is not null", label: "不为空" }
+  ],
+  string: [
+    { id: "<=", label: "小于等于" },
+    { id: ">=", label: "大于等于" },
+    { id: "<", label: "小于" },
+    { id: ">", label: "大于" },
+    { id: "!=", label: "不等于" },
+    { id: "=", label: "等于" },
+    { id: "in", label: "属于" },
+    { id: "not in", label: "不属于" },
+    { id: "contain", label: "包含" },
+    { id: "uncontain", label: "不包含" },
+    { id: "startwith", label: "以指定开始" },
+    { id: "startnot", label: "不以指定开始" },
+    { id: "endwith", label: "以指定字符结束" },
+    { id: "endnot", label: "不以指定字符结束" },
+    { id: "is null", label: "空" },
+    { id: "is not null", label: "不为空" }
+  ],
+  time: [
+    { id: "<=", label: "早于等于" },
+    { id: ">=", label: "晚于等于" },
+    { id: "<", label: "早于" },
+    { id: ">", label: "晚于" },
+    { id: "!=", label: "不等于" },
+    { id: "=", label: "等于" },
+    { id: "is null", label: "空" },
+    { id: "is not null", label: "不为空" }
+  ]
+};
